ce0035d9a6d3104e5fff937d6d7373f3eb127f08
A.SAI SRUJAN <saisrujan.191me101@nitk.edu.in>
2021-11-17 21:56:52 +0530

    added districts feature

--- Responses.py
+++ Responses.py
@@ -4,18 +4,7 @@ from pprint import pprint
 from datetime import datetime
 
 from requests import sessions
-r=requests.get('https://cdn-api.co-vin.in/api/v2/admin/location/states')
-states= r.json()['states']
-dic={}
-for state in states :
-    param={'state_id': state['state_id']}
-    url='https://cdn-api.co-vin.in/api/v2/admin/location/districts/{}'.format(state['state_id'])
-    j=requests.get(url)
-    districts= j.json()['districts']
-    for district in districts:
-        
-        name=district['district_name'].lower()
-        dic[name]=district['district_id']
+
 
 def sample_responses(input_text):
     user_message=str(input_text).lower()
@@ -28,28 +17,7 @@ def sample_responses(input_text):
         date_time=now.strftime("%d/%m/%y,%H:%M:%S")
 
         return str(date_time)
-    key_list=list(dic.keys())
-    if user_message in key_list:
-        sparam={'district_id':dic[user_message],'date':datetime.now().strftime('%d-%m-%Y')}
-
-        url1='https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict'
-        x=requests.get(url=url1,params=sparam)
-        p=x.json()['sessions']
-        output=""
-        for session in p:
-            output+='Name of the centre is '+session['name']+'\n'
-            output+='Address:'+session['address']+'\n'
-            output+='Opens from '+session['from']+' and closes at '+session['to']+'\n'
-            output+='Minimum age limit: '+str(session['min_age_limit'])+'\n'
-            output+='Name of the vaccine: '+session['vaccine']+'\n'
-            output+='Total available capacity: '+str(session['available_capacity'])+'\n'
-            output+='Total available capacity for dose 1: '+str(session['available_capacity_dose1'])+'\n'
-            output+='Total available capacity for dose 2: '+str(session['available_capacity_dose2'])+'\n'
-            if str(session['fee_type'])== 'paid':
-                output+='Fees :'+str(session['fee'])+'\n'
-            output+='\n'
-            break
-        return output
+    
 
 
 
--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import constants as keys
 from telegram.ext import *
 from telegram import *
 import Responses as R
+from datetime import datetime
+import requests
+
+
+dic=keys.dic
+
+
 
 print("bot started...")
 
@@ -13,9 +20,34 @@ def help_command(update,context):
 
 def handle_message(update,context):
     text=str(update.message.text).lower()
+    key_list=list(dic.keys())
+    if text in key_list:
+        output_district(update,context,text)
+        return
     response=R.sample_responses(text)
     update.message.reply_text(response)
 
+def output_district(update,context,text):
+    sparam={'district_id':dic[text],'date':datetime.now().strftime('%d-%m-%Y')}
+
+    url1='https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict'
+    x=requests.get(url=url1,params=sparam)
+    p=x.json()['sessions']
+    output=""
+    for session in p:
+        output=""
+        output+='Name of the centre is '+session['name']+'\n'
+        output+='Address:'+session['address']+'\n'
+        output+='Opens from '+session['from']+' and closes at '+session['to']+'\n'
+        output+='Minimum age limit: '+str(session['min_age_limit'])+'\n'
+        output+='Name of the vaccine: '+session['vaccine']+'\n'
+        output+='Total available capacity: '+str(session['available_capacity'])+'\n'
+        output+='Total available capacity for dose 1: '+str(session['available_capacity_dose1'])+'\n'
+        output+='Total available capacity for dose 2: '+str(session['available_capacity_dose2'])+'\n'
+        if str(session['fee_type'])== 'paid':
+            output+='Fees :'+str(session['fee'])+'\n'
+        output+='\n'
+        update.message.reply_text(output)
 def error(update,context):
     print(f"Update{update} caused error{context.error}")
 
